360ff7d5fdf5bec7db148aba37b1b75d864a27c5
FireBuddy <aijun@live.ca>
2016-07-25 20:11:32 -0400

    Update Brand.cs

--- KappaAIO/KappaAIO/Champions/Brand.cs
+++ KappaAIO/KappaAIO/Champions/Brand.cs
@@ -393,11 +393,11 @@
         private static void Obj_AI_Base_OnBasicAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             CurrentTarget = TargetSelector.GetTarget(W.Range, DamageType.Magical);
-
-            if ((CurrentTarget.Hero != Champion.Yasuo && sender.Mana <= 90) && Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) && sender == CurrentTarget && !sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(W.Range) && W.IsReady() && sender.IsEnemy)
+            //(CurrentTarget.Hero != Champion.Yasuo && sender.Mana <= 90)//
+            if ( Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) && sender == CurrentTarget && !sender.IsDashing() && sender.Type == GameObjectType.AIHeroClient && sender.IsValidTarget(W.Range) && W.IsReady() && sender.IsEnemy)
             {
 
-                    W.Cast( sender.ServerPosition);
+                    W.Cast(sender.ServerPosition);
                     Chat.Print("Basic Attack:"+args.SData.Name);
                     
 
