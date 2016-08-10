05fdc8ee2083aeefdc60b69314d6443302764e43
FireBuddy <aijun@live.ca>
2016-08-10 14:54:19 -0400

    Update Brand.cs

--- KappaAIO/KappaAIO/Champions/Brand.cs
+++ KappaAIO/KappaAIO/Champions/Brand.cs
@@ -31,8 +31,8 @@ namespace KappaAIO.Champions
         {
             try
             {
-                Q = new Spell.Skillshot(SpellSlot.Q, 1000, SkillShotType.Linear, 250, 1550, 60);
-                W = new Spell.Skillshot(SpellSlot.W, 900, SkillShotType.Circular, 650, -1, 250);
+                Q = new Spell.Skillshot(SpellSlot.Q, 1000, SkillShotType.Linear, 250, 1600, 60);
+                W = new Spell.Skillshot(SpellSlot.W, 900, SkillShotType.Circular, 850, -1, 100);
                 E = new Spell.Targeted(SpellSlot.E, 630);
                 R = new Spell.Targeted(SpellSlot.R, 750);
 
