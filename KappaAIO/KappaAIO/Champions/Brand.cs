1c813d7b4dc832b4060c762bebc96f66284ae6be
FireBuddy <aijun@live.ca>
2016-08-21 11:37:22 -0400

    Update Brand.cs

--- KappaAIO/KappaAIO/Champions/Brand.cs
+++ KappaAIO/KappaAIO/Champions/Brand.cs
@@ -408,7 +408,7 @@ namespace KappaAIO.Champions
         public override void Harass()
         {
           CurrentTarget = TargetSelector.GetTarget(W.Range, DamageType.Magical);
-          var minion = EntityManager.MinionsAndMonsters.GetLaneMinions().Where(a => a.Distance(Player.Instance) < E.Range && a.Distance(CurrentTarget.ServerPosition) < 150).OrderBy(a => a.Health).FirstOrDefault();
+          var minion = EntityManager.MinionsAndMonsters.GetLaneMinions().Where(a => a.Distance(Player.Instance) < E.Range && a.Distance(CurrentTarget.ServerPosition) < 150 && a.brandpassive()).OrderBy(a => a.Health).FirstOrDefault();
         }
 
         public override void LaneClear()
